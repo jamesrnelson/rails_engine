d2c3366dddaf774cf2b16d80f23480e9e5c966d6
Margaret Williford <margaretwilliford4@gmail.com>
2018-04-30 21:25:40 -0600

    find all merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -24,16 +24,29 @@ describe 'Merchants API' do
   end
 
   it 'can find single object for params' do
-    create_list(:merchant, 3)
+    create(:merchant, name: 'Coupes')
     name = 'The Virginian'
     create(:merchant, name: name)
 
     get "/api/v1/merchants/find?name=#{name}"
-
     expect(response).to be_success
 
     merchant = JSON.parse(response.body)
-
     expect(merchant['name']).to eq('The Virginian')
+  end
+
+  it 'can find all objects for params' do
+    create_list(:merchant, 3, name: 'The Lawn')
+    name = 'Rotunda'
+    create_list(:merchant, 5 ,name: name)
+
+    get "/api/v1/merchants/find_all?name=#{name}"
+    expect(response).to be_success
+
+    merchants = JSON.parse(response.body)
+    expect(merchants.count).to eq(5)
+    merchants.each do |merchant|
+      expect(merchant['name']).to eq(name)
+    end
   end
 end
