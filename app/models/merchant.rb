369fbee13f09387f862495d2f94963e39005d42a
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-01 15:36:47 -0600

    merchant revenue WIP

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :items
+
+  def revenue
+    invoice_items.
+    joins(:transactions).
+    where(status: "success").
+    sum('unit_price * quantity')
+  end
+
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
 
 describe 'Merchant' do
-  describe 'Relationships' do
+  before(:each) do
+    @merchant = create(:merchant)
+  end
+  context 'Relationships' do
     it { should have_many(:items) }
     it { should have_many(:invoices) }
   end
   context 'instance methods' do
     it 'can return total revenue' do
-      merchant = create(:merchant)
-      item_1 = create(:item, merchant_id: merchant.id)
-      item_2 = create(:item, merchant_id: merchant.id)
+      item_1 = create(:item, merchant_id: @merchant.id)
+      item_2 = create(:item, merchant_id: @merchant.id)
       invoice_item_1 = create(:invoice_item, item: item_1)
       invoice_item_2 = create(:invoice_item, item: item_1)
       invoice_item_3 = create(:invoice_item, item: item_2)
       invoice_item_4 = create(:invoice_item, item: item_2)
       invoice_item_5 = create(:invoice_item, item: item_2)
 
-      expect(merchant.revenue).to eq(15000)
+      expect(@merchant.revenue).to eq(15000)
     end
   end
 end
