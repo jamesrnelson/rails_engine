cedbfd0ee01d8fe2af57ebf9f9af8d616a4be0a9
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-01 19:04:37 -0600

    transaction index

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,16 @@ Rails.application.routes.draw do
         get 'find_all', to: 'search#index'
         get 'random', to: 'random#show'
       end
+      resources :merchants, only: [:index, :show]
+
+      resources :transactions, only: [:index]
+      
       namespace :invoices do
         get 'find', to: 'search#show'
         get 'find_all', to: 'search#index'
         get 'random', to: 'random#show'
       end
       resources :invoices, only: [:index, :show]
-      resources :merchants, only: [:index, :show]
 
       namespace :items do
         get '/:id/merchant', to: 'merchants#show'
---
+++ spec/requests/api/v1/transactions/transactions_request_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+describe 'Transactions API' do
+  it 'sends a list of transactions' do
+    create_list(:transaction, 3)
+
+    get '/api/v1/transactions'
+
+    expect(response).to be_success
+
+    transactions = JSON.parse(response.body)
+    expect(transactions.count).to eq(3)
+  end
+  #
+  # it 'can show an individual merchant' do
+  #   id = create(:merchant).id
+  #
+  #   get "/api/v1/merchants/#{id}"
+  #
+  #   expect(response).to be_success
+  #
+  #   merchant = JSON.parse(response.body)
+  #   expect(merchant['id']).to eq(id)
+  # end
+  #
+  # it 'can find single object for params' do
+  #   create(:merchant, name: 'Coupes')
+  #   name = 'The Virginian'
+  #   create(:merchant, name: name)
+  #
+  #   get "/api/v1/merchants/find?name=#{name}"
+  #   expect(response).to be_success
+  #
+  #   merchant = JSON.parse(response.body)
+  #   expect(merchant['name']).to eq('The Virginian')
+  # end
+  #
+  # it 'can find all objects for params' do
+  #   create_list(:merchant, 3, name: 'The Lawn')
+  #   name = 'Rotunda'
+  #   create_list(:merchant, 5 ,name: name)
+  #
+  #   get "/api/v1/merchants/find_all?name=#{name}"
+  #   expect(response).to be_success
+  #
+  #   merchants = JSON.parse(response.body)
+  #   expect(merchants.count).to eq(5)
+  #   merchants.each do |merchant|
+  #     expect(merchant['name']).to eq(name)
+  #   end
+  # end
+  #
+  # it 'can return a random merchant' do
+  #   create_list(:merchant, 10)
+  #
+  #   get '/api/v1/merchants/random'
+  #   expect(response).to be_success
+  #
+  #   merchant = JSON.parse(response.body)
+  #   expect(merchant.class).to eq(Hash)
+  #   expect(merchant.keys). to eq(['id', 'name'])
+  # end
+end
