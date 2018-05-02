80cb80ef8349cf0b3e5465740e0f542ac9e4cb4d
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-01 20:43:56 -0600

    invoice item see associated item

--- spec/requests/api/v1/invoice_items/relationships_spec.rb
+++ spec/requests/api/v1/invoice_items/relationships_spec.rb
@@ -13,4 +13,13 @@ describe 'Invoice Items API relationships' do
     invoice = JSON.parse(response.body)
     expect(invoice['id']).to eq(@invoice_item.invoice_id)
   end
+
+  it 'sends the associated item for a given invoice_item' do
+    get "/api/v1/invoice_items/#{@invoice_item.id}/item"
+
+    expect(response).to be_success
+
+    item = JSON.parse(response.body)
+    expect(item['id']).to eq(@invoice_item.item_id)
+  end
 end
