12caca8976ddaf4e3acc7ff9d53a8bc0a330f492
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-01 19:34:46 -0600

    transaction find all

--- spec/requests/api/v1/transactions/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions/transactions_request_spec.rb
@@ -35,21 +35,21 @@ describe 'Transactions API' do
     expect(transaction['result']).to eq('success')
   end
 
-  # it 'can find all objects for params' do
-  #   create_list(:merchant, 3, name: 'The Lawn')
-  #   name = 'Rotunda'
-  #   create_list(:merchant, 5 ,name: name)
-  #
-  #   get "/api/v1/merchants/find_all?name=#{name}"
-  #   expect(response).to be_success
-  #
-  #   merchants = JSON.parse(response.body)
-  #   expect(merchants.count).to eq(5)
-  #   merchants.each do |merchant|
-  #     expect(merchant['name']).to eq(name)
-  #   end
-  # end
-  #
+  it 'can find all objects for params' do
+    create_list(:transaction, 3, result: 'success')
+    result = 'failed'
+    create_list(:transaction, 5 , result: result)
+
+    get "/api/v1/transactions/find_all?result=#{result}"
+    expect(response).to be_success
+
+    transactions = JSON.parse(response.body)
+    expect(transactions.count).to eq(5)
+    transactions.each do |transaction|
+      expect(transaction['result']).to eq(result)
+    end
+  end
+
   # it 'can return a random merchant' do
   #   create_list(:merchant, 10)
   #
