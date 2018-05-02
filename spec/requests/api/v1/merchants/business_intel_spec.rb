5942e13f7fa5660fb064ced2958725c0f27aaff1
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-02 16:02:35 -0600

    single merchant revenue endpoint

--- spec/requests/api/v1/merchants/business_intel_spec.rb
+++ spec/requests/api/v1/merchants/business_intel_spec.rb
@@ -3,12 +3,22 @@ require 'rails_helper'
 describe 'Merchants API' do
   it 'returns the total revenue for that merchant across successful transactions' do
     merchant = create(:merchant)
+    item_1 = create(:item, merchant_id: merchant.id)
+    item_2 = create(:item, merchant_id: merchant.id)
+    invoice = create(:invoice)
+    invoice_item_1 = create(:invoice_item, item: item_1, invoice: invoice)
+    invoice_item_2 = create(:invoice_item, item: item_1, invoice: invoice)
+    invoice_item_3 = create(:invoice_item, item: item_2, invoice: invoice)
+    invoice_item_4 = create(:invoice_item, item: item_2, invoice: invoice)
+    invoice_item_5 = create(:invoice_item, item: item_2, invoice: invoice)
+    transaction = create(:transaction, invoice: invoice)
 
     get "/api/v1/merchants/#{merchant.id}/revenue"
 
     expect(response).to be_success
 
     revenue = JSON.parse(response.body)
-    expect(revenue['revenue']).to eq(merchant.revenue.to_s)
+    expected = '%.2f' % (merchant.revenue.to_f / 100)
+    expect(revenue['revenue']).to eq(expected)
   end
 end
