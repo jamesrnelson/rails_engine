d0c92999064f0bfcde0b067e30a2065989b2ba1a
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-01 21:03:35 -0600

    WIP

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,8 +3,8 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :invoice_items, through: :items
 
-  def revenue
-    invoice_items.
+  def revenue(params={})
+    invoice_items.where(params)
     joins(:transactions).
     where(status: "success").
     sum('unit_price * quantity')
