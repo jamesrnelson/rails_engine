5dbbf54e27af7ba51daf2139253ef92c492540ae
JNelson <nelson.jimmy@gmail.com>
2018-05-03 11:13:01 -0600

    Resolved Item unit_price issues

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -10,6 +10,9 @@ class Api::V1::Items::SearchController < ApplicationController
   private
 
   def item_params
+    if params[:unit_price]
+      params[:unit_price] = params[:unit_price].delete('.')
+    end
     params.permit(
       :id,
       :name,
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,10 @@
+require './lib/formatters/money_formatter'
+
 class ItemSerializer < ActiveModel::Serializer
+  include MoneyFormatter
   attributes :description, :id, :merchant_id, :name, :unit_price
 
   def unit_price
-    '%.2f' % (object.unit_price.to_f / 100)
+    format_money(object.unit_price)
   end
 end
