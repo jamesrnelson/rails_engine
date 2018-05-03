63a3e5bd4052cb00bc66921da1a446a1c1cf3609
JNelson <nelson.jimmy@gmail.com>
2018-05-03 07:53:44 -0600

    Implemented Invoice Item Search Controller find all

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::InvoiceItems::SearchController < ApplicationController
     render json: InvoiceItem.find_by(invoice_item_params)
   end
 
+  def index
+    render json: InvoiceItem.where(invoice_item_params)
+  end
+
   private
 
   def invoice_item_params
