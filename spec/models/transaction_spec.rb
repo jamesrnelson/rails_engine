acc29ab32148f98484195832f309b04ad763d782
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-02 19:11:15 -0600

    updating model tests to use shouldamatchers

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 describe 'Transaction' do
   context 'relationships' do
-    it 'belongs to an invoice' do
-      transaction = create(:transaction)
-      expect(transaction).to respond_to(:invoice)
-    end
+    it { should belong_to(:invoice) }
   end
 end
