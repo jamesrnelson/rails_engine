f1184bc7eef20dae2f625a4874bd3a152ad95eb2
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-02 19:48:55 -0600

    aligning with spec harness, looking for date on invoice instead of invoice item

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::Merchants::RevenueController < ApplicationController
   def search_params
     if params[:date]
       date = Date.parse(params[:date])
-      {created_at: date.beginning_of_day..date.end_of_day}
+      {invoices: {created_at: date.beginning_of_day..date.end_of_day} }
     end
   end
 
