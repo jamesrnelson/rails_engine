444cf3545d340035eb556abd8c6238b9a5e278de
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-03 18:32:21 -0600

    passing rspec

--- spec/requests/api/v1/customers/business_intelligence_spec.rb
+++ spec/requests/api/v1/customers/business_intelligence_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 describe 'Customers API' do
   it 'returns a merchant where the customer has conducted the most successful transactions' do
     customer = create(:customer)
-    get "/api/v1/customers/#{customer.id}/favorite_merchant"
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+    create(:transaction, invoice: invoice)
 
+    get "/api/v1/customers/#{customer.id}/favorite_merchant"
+    expect(response).to be_success
+
+    fav_merchant = JSON.parse(response.body)
+    expect(fav_merchant['id']).to eq(customer.favorite_merchant.id)
+    expect(fav_merchant['id']).to eq(merchant.id)
   end
 end
